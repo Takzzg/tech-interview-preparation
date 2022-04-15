9800b4d719b4d7b47aeabf82eaaf939f905ae721
Guido Queiroz <Takzzg@hotmail.com>
2022-04-15 01:37:57 -0300

    npm files update

--- Ejercicios/01-FindNeedle/FindNeedle.js
+++ Ejercicios/01-FindNeedle/FindNeedle.js
@@ -1,34 +1,19 @@
-function FindNeedle (haystack, needle) {
-  // Your code here:
-
-  console.log(haystack)
-  //                               ^
-  console.log(needle)
-
-  // for (let i = 0; i < haystack.length; i++) {
-  //   for (let j = 0; j < needle.length; j++) {
-  //     if (needle[j] !== haystack[i + j]) break
-  //     if (j === needle.length - 1) return i
-  //   }
-  // }
-  // return -1
-
-  // O(n*m)
-  // n = haystack.length
-  // m = needle.length
-
-  for (let i = 0; i < haystack.length; i++) {
-    console.log(haystack.slice(i, i + needle.length))
-
-    if (haystack.slice(i, i + needle.length) === needle) return i
-  }
-  return -1
-
-  // O(n*m)
+function FindNeedle(haystack, needle) {
+    for (let i = 0; i < haystack.length - needle.length + 1; i++) {
+        if (haystack.slice(i, i + needle.length) === needle) {
+            console.log(
+                `${haystack} -> ${needle} \n` +
+                    haystack.slice(0, i) +
+                    "(" +
+                    needle.toUpperCase() +
+                    ")" +
+                    haystack.slice(i + needle.length)
+            )
+            return i
+        }
+    }
+    console.log(`${haystack} -> ${needle} \n-1`)
+    return -1
 }
 
 module.exports = FindNeedle
-
-console.log(FindNeedle('react-redux', 'redux')) // 6
-// console.log(FindNeedle("rereredux", "reredux")) // 2
-// console.log(FindNeedle("pinky", "puntual")) // -1
