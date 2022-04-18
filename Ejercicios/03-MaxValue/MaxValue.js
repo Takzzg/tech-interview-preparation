0a7335f59e1e9727dc3089f320ee34e01cb8cb91
Guido Queiroz <Takzzg@hotmail.com>
2022-04-18 16:41:45 -0300

    03

--- Ejercicios/03-MaxValue/MaxValue.js
+++ Ejercicios/03-MaxValue/MaxValue.js
@@ -1,41 +1,11 @@
-function MaxValue (shares) {
-  let maxDif = shares[1] - shares[0]
-  for (let i = 0; i < shares.length; i++) {
-    const compra = shares[i]
-    console.log(compra)
-    for (let j = i + 1; j < shares.length; j++) {
-      const venta = shares[j]
-      const currentDif = venta - compra
-      if (currentDif > maxDif) maxDif = currentDif
-    }
-  }
-  return maxDif
+function MaxValue(shares) {
+    let min = [...shares].sort((a, b) => a - b)[0]
+    let max = [...shares]
+        .splice(shares.indexOf(min))
+        .sort((a, b) => a - b)
+        .pop()
 
-
-// Complejidad O(n^2)
-
-  // Solucion Óptima:
-  // let maxDif = shares[1] - shares[0]
-  let minPrice = shares[0]
-  // console.log(minPrice)
-  shares.forEach(stock => {
-    // console.log(stock)
-    // minPrice = Math.min(minPrice, stock)
-    // minPrice = stock < minPrice ? stock : minPrice
-    if (stock < minPrice) minPrice = stock
-    // console.log(minPrice)
-    const actualDif = stock - minPrice
-    // console.log(actualDif)
-    // maxDif = Math.max(maxDif, actualDif)
-    // maxDif = actualDif > maxDif ? actualDif : maxDif
-    if (actualDif > maxDif) maxDif = actualDif
-    // console.log(maxDif)
-  })
-  return maxDif
+    return max - min
 }
 
 module.exports = MaxValue
-
-// console.log(MaxValue([23, 7, 3, 4, 8, 6])) // 5
-// console.log(MaxValue([4, 3, 2, 5, 11])) // 9
-console.log(MaxValue([-4, -23, -1, -25, -11]))
