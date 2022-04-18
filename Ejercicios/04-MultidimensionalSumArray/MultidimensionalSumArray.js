2ad8b2fecb3946d739da0c187d6628365b12124a
Guido Queiroz <Takzzg@hotmail.com>
2022-04-18 17:19:20 -0300

    04

--- Ejercicios/04-MultidimensionalSumArray/MultidimensionalSumArray.js
+++ Ejercicios/04-MultidimensionalSumArray/MultidimensionalSumArray.js
@@ -1,28 +1,10 @@
 function mdArraySum(arr) {
+    if (!Array.isArray(arr)) return arr
 
-  let acc = 0
-  for (const elm of arr) {
-    console.log(elm)
-    if (typeof elm === 'number') acc = acc + elm
-    else acc = acc + mdArraySum(elm)
-  }
-  return acc
+    let sum = 0
+    for (let i = 0; i < arr.length; i++) sum += mdArraySum(arr[i])
 
-  // for (const num of arr) {
-  //   if (typeof num === 'number') acc += num
-  //   else acc = mdArraySum(num, acc)
-  // }
-  // return acc
-
-  for (const num of arr) {
-  acc = typeof num === 'number' ? acc + num : mdArraySum(num, acc)
-  }
-
-  return arr.reduce((acc, elm) => acc + (typeof elm === 'number' ? elm : mdArraySum(elm)), 0)
-
-  return acc
-  //O(n)
-
+    return sum
 }
 
-module.exports = mdArraySum;
+module.exports = mdArraySum
